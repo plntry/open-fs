093a32fecbcc4a6772cb557f31bc2ced6760e32b
Oksana Nedilko <nedilko7b@gmail.com>
2022-04-15 17:32:36 +0300

    unicafe-step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,17 +13,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
 
   const handleGoodClick = () => {
+    setAll(allClicks.concat(1))
     setGood(good + 1)
   }
 
   const handleNeutralClick = () => {
+    setAll(allClicks.concat(0))
     setNeutral(neutral + 1)
   }
 
   const handleBadClick = () => {
+    setAll(allClicks.concat(-1))
     setBad(bad + 1)
+  }
+
+  const sumClicks = (array) => {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i]
+    }
+    return sum
   }
 
   return (
@@ -36,6 +48,9 @@ const App = () => {
       <Display value={good} text='good' />
       <Display value={neutral} text='neutral' />
       <Display value={bad} text='bad' />
+      <Display value={good + neutral + bad} text='all' />
+      <Display value={sumClicks(allClicks) / allClicks.length} text='average' />
+      <Display value={good * 100 / (good + neutral + bad) + "%"} text='positive' />
     </div>
   )
 }
