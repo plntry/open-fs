2fe5fa40e41f27f5c7bbdf127c1b2a202e57434f
Oksana Nedilko <nedilko7b@gmail.com>
2022-04-17 18:50:27 +0300

    courseinfo-step6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,61 @@
+const Header = ({ course }) => <h1>{course.name}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ course }) => {
+  const parts = course.parts
+  return (
+    <>
+      {parts.map(part =>
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      )}
+    </>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <>
+      <Part course={course} />
+    </>
+  )
+}
+  
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course} />
+      <Content course={course} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course}/>
+}
+
+export default App
