221d113b0b9c7e51dfe68fe58cf521426aca9ffd
Oksana Nedilko <nedilko7b@gmail.com>
2022-05-07 16:33:18 +0300

    phonebook backend step 2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -23,12 +23,16 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-  
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>
+  `)
 })
 
 const PORT = 3001
