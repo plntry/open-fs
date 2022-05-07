ea002718ddc478d4aa6c5e883d07db82e1ca9ce3
Oksana Nedilko <nedilko7b@gmail.com>
2022-05-07 16:59:37 +0300

    phonebook backend step 4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,27 +1,28 @@
+const { request, response } = require('express')
 const express = require('express')
 const app = express()
 
 let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
+  { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+  },
+  { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
     },
     { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+  }
 ]
 
 app.get('/api/persons', (request, response) => {
@@ -43,6 +44,13 @@ app.get('/info', (request, response) => {
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${Date()}</p>
   `)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
