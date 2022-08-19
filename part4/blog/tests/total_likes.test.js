430966a68410abb5a099009b157b559f92423880
Oksana Nedilko <nedilko7b@gmail.com>
2022-08-19 17:54:50 +0300

    4.4: helper functions and unit tests, step2

---
+++ part4/blog/tests/total_likes.test.js
@@ -0,0 +1,58 @@
+const listHelper = require('../utils/list_helper')
+
+describe('total likes', () => {
+    const blogsEmptyList = []
+
+    test('when blog list is empty, equals 0', () => {
+        const result = listHelper.totalLikes(blogsEmptyList)
+        expect(result).toBe(0)
+    })
+
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        expect(result).toBe(5)
+    })
+
+    const blogsList = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f9',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 11,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f7',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 8,
+            __v: 0
+        }
+    ]
+
+    test('when list has more than 1 blog, equels the sum of likes', () => {
+        const result = listHelper.totalLikes(blogsList)
+        expect(result).toBe(24)
+    })
+})
