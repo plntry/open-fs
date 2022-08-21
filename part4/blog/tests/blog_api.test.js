bc1cbc0d5a8917b8116a5fab84234298e56b470a
Oksana Nedilko <nedilko7b@gmail.com>
2022-08-21 14:56:55 +0300

    4.8: Blog list tests, step1

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const helper = require('./helpers/test_helper')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    for (let blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
